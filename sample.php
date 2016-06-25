4898d212658551de45facf60cf4910df8f880d09
Masoud Amini <amini141@gmail.com>
2016-06-25 15:10:07 +0430

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,24 +1,24 @@
 <?php
 
 
-$merchantID = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+$MerchantID = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
 
 // log GET & SERVER parameters For Test
 // file_put_contents('test.txt', 'Server:' . PHP_EOL . print_r($_SERVER, true) . PHP_EOL . ' Get:' . PHP_EOL . print_r($_GET, true), FILE_APPEND);
 
 if (strlen($_GET['Authority']) == 36 && $_GET['Status'] == 'OK') {
-    $amount = $_SERVER['HTTP_X_PAY_AMOUNT'];
+    $Amount = $_SERVER['HTTP_X_PAY_AMOUNT'];
     $payerCell = $_SERVER['HTTP_X_PAY_MOBILE'];
     if (!empty($amount) && !empty($payerCell)) {
         // URL also Can be https://de.zarinpal.com/pg/services/WebGate/wsdl
-       $client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
+       $client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); 
 		
 		$result = $client->PaymentVerification(
-						  	array(
+						  	[
 									'MerchantID'	 => $MerchantID,
-									'Authority' 	 => $Authority,
+									'Authority' 	 => $_GET['Authority'],
 									'Amount'	 => $Amount
-								)
+								]
 		);
 
         if ($result->Status == 100) {
